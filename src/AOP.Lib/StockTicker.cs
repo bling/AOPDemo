d24f9e638f9044722f0b789f40c07407c8b67320
bling <bling@live.ca>
2011-04-16 16:49:19 -0400

    .lib got ignored, add project back in, replace with code from dependencypropertyweaver

---
+++ src/AOP.Lib/StockTicker.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace AOP.Lib
+{
+    public class StockTickerInpc : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public class StockTickerPoco
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/AOP.Lib/StockTickerVerbose.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace AOP.Lib
+{
+    public class StockTickerVerbose : DependencyObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    var e = PropertyChanged;
+                    if (e != null)
+                        e(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        private static readonly DependencyProperty PriceDependencyProperty =
+            DependencyProperty.Register("Price", typeof(decimal), typeof(StockTickerVerbose));
+
+        public decimal Price
+        {
+            get { return (decimal)GetValue(PriceDependencyProperty); }
+            set { SetValue(PriceDependencyProperty, value); }
+        }
+    }
+}
